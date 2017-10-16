43f13c0de7ae88c2b153456399374a20f448d3a9
Outrider99 <outrider.w@gmail.com>
2017-10-16 13:10:43 -0600

    Added a third screen for practice

---
+++ NewPracticeProject/Controller/ThirdScreenController.swift
@@ -0,0 +1,14 @@
+//
+//  ThirdScreenController.swift
+//  NewPracticeProject
+//
+//  Created by Miller, Wyatt on 10/16/17.
+//  Copyright © 2017 Miller, Wyatt. All rights reserved.
+//
+
+import UIKit
+
+public class ThirdScreenController : UIViewController
+{
+    
+}
