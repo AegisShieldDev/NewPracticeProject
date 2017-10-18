91720500028e5ecf81c1695b08e4b5b3b837e090
Outrider99 <outrider.w@gmail.com>
2017-10-18 12:23:59 -0600

    Added buttons, slider, images

--- NewPracticeProject/Controller/ThirdScreenController.swift
+++ NewPracticeProject/Controller/ThirdScreenController.swift
@@ -11,4 +11,18 @@ import UIKit
 public class ThirdScreenController : UIViewController
 {
     
+    @IBOutlet weak var buttonOneOutput: UIButton!
+    @IBOutlet weak var buttonTwoOutput: UIButton!
+    @IBOutlet weak var sliderOutput:    UISlider!
+    @IBOutlet weak var imageOutput:     UIImageView!
+    
+    private lazy var color : ColorTools = ColorTools()
+    private var ImageCounter : Int = 0
+    
+    @IBAction func ButtonOneAction(_ sender: Any) {
+    }
+    @IBAction func ButtonTwoAction(_ sender: Any) {
+    }
+    @IBAction func SliderAction(_ sender: Any) {
+    }
 }
